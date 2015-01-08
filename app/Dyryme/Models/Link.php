0d832447a31d48cc8ff5824102ac8456c5c628ff
Michael Dyrynda <deringer@iatstuti.net>
2015-01-08 20:24:44 +1030

    Add link model

---
+++ app/Dyryme/Models/Link.php
@@ -0,0 +1,18 @@
+<?php namespace app/
+Dyryme;
+
+use Eloquent;
+
+/**
+ * Link model
+ *
+ * @package    Dyryme
+ * @subpackage Models
+ * @copyright  2015 IATSTUTI
+ * @author     Michael Dyrynda <michael@iatstuti.net>
+ */
+class Link extends Eloquent {
+
+	protected $fillable = [ 'hash', 'url', 'remoteAddress', 'hostname', 'userAgent' ];
+
+}
