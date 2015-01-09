7bae3c4aec7887bec4665ec712f86ad6d78c1bf3
Michael Dyrynda <deringer@iatstuti.net>
2015-01-10 00:03:55 +1030

    Set length on hash field

--- app/database/migrations/2015_01_05_204818_create_links_table.php
+++ app/database/migrations/2015_01_05_204818_create_links_table.php
@@ -15,7 +15,7 @@ class CreateLinksTable extends Migration {
         Schema::create('links', function (Blueprint $table)
         {
             $table->increments('id');
-            $table->string('hash')->unique();
+            $table->string('hash', 5)->unique();
             $table->string('url')->unique();
             $table->string('remoteAddress')->nullable();
             $table->string('hostname')->nullable();
