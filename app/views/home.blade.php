baa86f0dee02cb6a77f5696302ee4424960f1bef
Michael Dyrynda <deringer@iatstuti.net>
2015-01-10 02:31:19 +1030

    Update display of flash messages

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -10,8 +10,8 @@
         </div>
         {{ Form::close() }}
 
-        @if ( isset($hash) )
-            <output>{{ link_to($hash) }}</output>
+        @if ( Session::has('hash') )
+            <output>{{ link_to(Session::get('hash')) }}</output>
         @endif
     </div>
 @stop
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -17,10 +17,10 @@
     <![endif]-->
   </head>
   <body>
-    @if ( isset($flash_message) )
+    @if ( Session::has('flash_message') )
       <div class="alert alert-info alert-dismissable">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-        {{ $flash_message }}
+        {{ Session::get('flash_message') }}
       </div>
     @endif
 
