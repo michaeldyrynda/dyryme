ec424f2c0847d703db909c6c406d43f5fc2bab58
Michael Dyrynda <deringer@iatstuti.net>
2015-01-10 17:42:43 +1030

    Add migration for hit log

---
+++ app/database/migrations/2015_01_10_174136_create_hit_log.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateHitLog extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('hit_log', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('link_id')->unsigned();
+			$table->bigInteger('remoteAddress')->nullable();
+			$table->string('hostname')->nullable();
+			$table->string('userAgent')->nullable();
+			$table->string('referrer')->nullable();
+			$table->nullableTimestamps();
+
+			$table->foreign('link_id')->references('id')->on('links')->onDelete('cascade')->onUpdate('cascade');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('hit_log');
+	}
+
+}
