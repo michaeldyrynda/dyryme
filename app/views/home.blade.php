07fe85e4a079dae509cd80fb79cf5859a0267b31
Michael Dyrynda <deringer@iatstuti.net>
2015-01-11 00:18:39 +1030

    Error will still be in url, just change input name

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -4,9 +4,9 @@
     <div class="col-sm-4 col-sm-offset-4">
         <h2 class="text-center">dyry.me link shortener</h2>
         {{ Form::open(array( 'route' => 'store', 'method' => 'post', )) }}
-        <div class="form-group @if ( $errors->has('longUrl') ) has-error @endif">
+        <div class="form-group @if ( $errors->has('url') ) has-error @endif">
             {{ Form::text('longUrl', null, array( 'class' => 'form-control input-lg', 'id' => 'url', 'placeholder' => 'Enter URL to shorten then press enter', )) }}
-            {{ $errors->first('longUrl', '<span class="help-block">:message</span>') }}
+            {{ $errors->first('url', '<span class="help-block">:message</span>') }}
         </div>
         {{ Form::close() }}
 
