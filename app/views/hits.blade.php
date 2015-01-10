b8a16364f842d26baadefa4775bf587b5a630f1e
Michael Dyrynda <deringer@iatstuti.net>
2015-01-11 08:21:32 +1030

    s/count/getTotal

--- app/views/hits.blade.php
+++ app/views/hits.blade.php
@@ -4,7 +4,7 @@
     <div class="col-md-12">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <h2 class="panel-title">Hits for {{ $link->hash }} - {{ $link->url }} ({{ $hits->count() }})</h2>
+                <h2 class="panel-title">Hits for {{ $link->hash }} - {{ $link->url }} ({{ $hits->getTotal() }})</h2>
             </div>
 
             <div class="table-responsive">
