07fff61f355269bb5386418589c7bd0cf34ee2bd
Michael Dyrynda <deringer@iatstuti.net>
2015-01-10 18:21:55 +1030

    Add list route

--- app/routes.php
+++ app/routes.php
@@ -14,3 +14,4 @@
 Route::get('/', [ 'as' => 'create', 'uses' => 'Dyryme\Controllers\LinkController@create', ]);
 Route::post('store', [ 'as' => 'store', 'uses' => 'Dyryme\Controllers\LinkController@store', ]);
 Route::get('{hash}', [ 'as' => 'redirect', 'uses' => 'Dyryme\Controllers\LinkController@redirect', ]);
+Route::get('list', [ 'as' => 'list', 'before' => 'auth', 'uses' => 'Dyryme\Controllers\LinkController@list', ]);
