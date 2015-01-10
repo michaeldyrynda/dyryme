2dd533d901131aa64003868cfb53470f4fc40fc0
Michael Dyrynda <deringer@iatstuti.net>
2015-01-10 20:38:56 +1030

    Make table responsive

--- app/views/list.blade.php
+++ app/views/list.blade.php
@@ -7,22 +7,24 @@
                 <h2 class="panel-title">Stored Links</h2>
             </div>
 
-            <table class="table table-striped table-condensed">
-                <thead>
-                <tr>
-                    <th>Hash</th>
-                    <th>URL</th>
-                    <th>Created</th>
-                    <th>IP Address</th>
-                    <th>Hostname</th>
-                    <th>User Agent</th>
-                    <th>Options</th>
-                </tr>
-                </thead>
-                <tbody>
-                @each('_link_row', $links, 'link', 'raw|<tr><th colspan="7">No links in the database</th></tr>')
-                </tbody>
-            </table>
+            <div class="table-responsive">
+                <table class="table table-striped table-condensed">
+                    <thead>
+                    <tr>
+                        <th>Hash</th>
+                        <th>URL</th>
+                        <th>Created</th>
+                        <th>IP Address</th>
+                        <th>Hostname</th>
+                        <th>User Agent</th>
+                        <th>Options</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @each('_link_row', $links, 'link', 'raw|<tr><th colspan="7">No links in the database</th></tr>')
+                    </tbody>
+                </table>
+            </div>
 
             {{ $links->links() }}
         </div>
