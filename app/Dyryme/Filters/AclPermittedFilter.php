35625d9f321c26a7e1eaa44684d1889c193e0c3f
Michael Dyrynda <deringer@iatstuti.net>
2015-01-19 20:24:24 +1030

    Add ACL permitted filter

---
+++ app/Dyryme/Filters/AclPermittedFilter.php
@@ -0,0 +1,51 @@
+<?php namespace Dyryme\Filters;
+
+use Dyryme\Repositories\UserRepository;
+
+/**
+ * ACL filter
+ *
+ * @package    Dyryme
+ * @subpackage Filters
+ * @copyright  2015 IATSTUTI
+ * @author     Michael Dyrynda <michael@iatstuti.net>
+ */
+class AclPermittedFilter {
+
+	/**
+	 * @var UserRepository
+	 */
+	private $userRepository;
+
+
+	/**
+	 * @param UserRepository $userRepository
+	 */
+	public function __construct(UserRepository $userRepository)
+	{
+		$this->userRepository = $userRepository;
+	}
+
+
+	/**
+	 * @param $route
+	 *
+	 * @return bool|\Illuminate\Http\RedirectResponse
+	 */
+	public function filter($route)
+	{
+		$user = $this->userRepository->getPermissions(\Auth::user()->id);
+
+		foreach ($user->groups as $group)
+		{
+			if ( $group->permissions->has($route->getName()) )
+			{
+				return true;
+			}
+		}
+
+		return \Redirect::route('user.denied');
+	}
+
+
+}
