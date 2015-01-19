f4cfd5a56e22f34120a2404da445681f0b2c50b6
Michael Dyrynda <deringer@iatstuti.net>
2015-01-19 20:25:34 +1030

    Add user repository

---
+++ app/Dyryme/Repositories/UserRepository.php
@@ -0,0 +1,63 @@
+<?php namespace Dyryme\Repositories;
+
+/**
+ * User repository
+ *
+ * @package    Dyryme
+ * @subpackage Repositories
+ * @copyright  2015 IATSTUTI
+ * @author     Michael Dyrynda <michael@iatstuti.net>
+ */
+class UserRepository {
+
+	/**
+	 * @var \User
+	 */
+	protected $model;
+
+
+	/**
+	 * @param \User $model
+	 */
+	function __construct(\User $model)
+	{
+		$this->model = $model;
+	}
+
+
+	/**
+	 * Create a new user
+	 *
+	 * @param array $input
+	 *
+	 * @return static
+	 */
+	public function store(array $input)
+	{
+		return $this->model->create($input);
+	}
+
+
+	/**
+	 * @param $userId
+	 *
+	 * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Support\Collection|static
+	 */
+	public function getUserWithLinks($userId)
+	{
+		return $this->model->with('links')->findOrFail($userId);
+	}
+
+
+	/**
+	 * @param $userId
+	 *
+	 * @return \Illuminate\Database\Eloquent\Collection|static[]
+	 */
+	public function getUserPermissions($userId)
+	{
+		return $this->model->with('groups', 'group.permissions')->get();
+	}
+
+
+}
