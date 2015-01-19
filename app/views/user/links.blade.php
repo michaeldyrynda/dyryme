9b71603c099de29be4e9ad8ffe21cc87dc13ad52
Michael Dyrynda <deringer@iatstuti.net>
2015-01-19 20:24:50 +1030

    Add user create, user links views

---
+++ app/views/user/links.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Stored Links ({{ $links->getTotal() }})</h2>
+                </div>
+
+                <div class="table-responsive">
+                    <table class="table table-striped table-condensed">
+                        <col style="width: 5%;"/>
+                        <col style="width: 15%;"/>
+                        <col style="width: 20%;"/>
+                        <col style="width: 10%;"/>
+                        <col style="width: 10%;"/>
+                        <col style="width: 35%;"/>
+                        <col style="width: 5%;"/>
+                        <thead>
+                        <tr>
+                            <th>Hash</th>
+                            <th>URL</th>
+                            <th>Description</th>
+                            <th>Created</th>
+                            <th>
+                                IP Address<br/>
+                                Hostname
+                            </th>
+                            <th>User Agent</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @each('_link_row', $links, 'link', 'raw|<tr><th colspan="7">No links in the database</th></tr>')
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+
+            {{ $links->links() }}
+        </div>
+    </div>
+@stop
