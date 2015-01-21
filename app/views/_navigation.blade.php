8fc3d8e44adc7933461616d0a6538afda1189a37
Michael Dyrynda <michael@nucleus.com.au>
2015-01-22 08:30:04 +1030

    Add collapsible navigation

--- app/views/_navigation.blade.php
+++ app/views/_navigation.blade.php
@@ -1,6 +1,12 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
         <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
             {{ link_to_route('create', 'dyry.me', null, [ 'class' => 'navbar-brand', ]) }}
         </div>
 
@@ -12,9 +18,17 @@
                 @endif
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li>@if ( Auth::check() ) <p class="navbar-text">Signed in as {{ Auth::user()->username }}</p> @else {{ link_to_route('register', 'Register') }}@endif</li>
-                @if ( Auth::guest() )<li>{{ link_to_route('login', 'Login') }}</li>@endif
-                @if ( Auth::check() )<li><a href="{{ route('logout') }}"><span class="glyphicon glyphicon-off"></span></a></li>@endif
+                @if ( Auth::check() )
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Signed in as {{ Auth::user()->username }} <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menue">
+                            <li><a href="{{ route('logout') }}"><span class="glyphicon glyphicon-off"></span>&nbsp;Logout</a></li>
+                        </ul>
+                    </li>
+                @else
+                    <li>{{ link_to_route('login', 'Login') }}</li>
+                    <li>{{ link_to_route('register', 'Register') }}</li>
+                @endif
             </ul>
         </div>
     </div>
