c14ba2ce434e85d2d7fa8ad14921a0bb963402a7
Michael Dyrynda <michael@nucleus.com.au>
2015-01-22 12:25:54 +1030

    Add recursion checks to try and make sure external referers don't send dyry.me into a redirect loop (i.e. tinyurl -> dyry.me -> tinyurl)

--- app/errors.php
+++ app/errors.php
@@ -4,3 +4,8 @@ App::error(function (\Dyryme\Exceptions\PermissionDeniedException $e)
 {
 	return \Redirect::route('user.denied');
 });
+
+App::error(function (\Dyryme\Exceptions\LooperException $e)
+{
+	return \Redirect::route('loop_detected');
+});
