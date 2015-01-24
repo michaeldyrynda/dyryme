b6ecaff0a67c82a0b3c21d4daf74ea005dfb5edf
Michael Dyrynda <deringer@iatstuti.net>
2015-01-24 18:24:10 +1030

    Change display of errors again, use input group to get url input and submit button on one line

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -5,20 +5,19 @@
         <div class="col-md-4 col-md-offset-4">
             <h2 class="text-center">dyry.me link shortener</h2>
             {{ Form::open(array( 'route' => 'store', 'method' => 'post', )) }}
-            @if ( ! $errors->isEmpty() )
-                @include('_alert', [ 'alert_type' => 'danger', 'alert_messages' => $errors, ])
-            @endif
-            <div class="form-group @if ( $errors->has('url') )has-error has-feedback @endif">
-                {{ Form::text('longUrl', null, array( 'class' => 'form-control input-lg', 'id' => 'url', 'placeholder' => 'Enter URL to shorten', 'required' => 'required', 'aria-describedby' => 'urlErrorStatus', )) }}
+            <div class="form-group @if ( $errors->has('url') )has-error @endif">
+                <div class="input-group input-group-lg">
+                    {{ Form::text('longUrl', null, array( 'class' => 'form-control input-lg', 'id' => 'url', 'placeholder' => 'Enter URL to shorten', 'required' => 'required', 'aria-describedby' => 'urlErrorStatus', )) }}
+                    <span class="input-group-btn">
+                        {{ Form::button('Submit', array( 'class' => 'btn btn-lg btn-default', 'type' => 'submit', )) }}
+                    </span>
+                </div>
+
                 @if ( $errors->has('url') )
-                    <span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>
-                    <span id="urlErrorStatus" class="sr-only">(error)</span>
+                    {{ $errors->first('url', '<span class="help-block">:message</span>') }}
                 @endif
             </div>
 
-            <div class="controls">
-                {{ Form::button('Submit', array( 'class' => 'btn btn-default', 'type' => 'Submit', )) }}
-            </div>
             {{ Form::close() }}
 
             @if ( Session::has('hash') )
