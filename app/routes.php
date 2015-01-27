2b0147125e037c968cf9d96c0421c3dde922b00b
Michael Dyrynda <michael@nucleus.com.au>
2015-01-27 11:56:21 +1030

    Group routes, update composer

--- app/routes.php
+++ app/routes.php
@@ -21,33 +21,36 @@ Route::post('queue/receive', function()
 	return Queue::marshal();
 });
 
-Route::get('/', [ 'as' => 'create', 'uses' => 'Dyryme\Controllers\LinkController@create', ]);
-Route::post('store', [ 'as' => 'store', 'before' => 'csrf', 'uses' => 'Dyryme\Controllers\LinkController@store', ]);
-Route::get('screenshot/{id}', [ 'as' => 'screenshot', 'uses' => 'Dyryme\Controllers\LinkController@screenshot', ]);
-
-Route::get('looper', [ 'as' => 'loop_detected', 'uses' => 'Dyryme\Controllers\LinkController@looper', ]);
-
-// Authenticated link routes
-Route::group([ 'prefix' => 'link', 'before' => 'auth', ], function ()
+Route::group([ 'namespace' => 'Dyryme\Controllers', ], function()
 {
-	Route::get('list', [ 'as' => 'list', 'uses' => 'Dyryme\Controllers\LinkController@index', ]);
-	Route::delete('{id}', [ 'as' => 'link.destroy', 'before' => 'auth|csrf', 'uses' => 'Dyryme\Controllers\LinkController@destroy', ]);
-	Route::put('{id}', [ 'as' => 'link.activate', 'before' => 'auth|csrf', 'uses' => 'Dyryme\Controllers\LinkController@activate', ]);
-	Route::get('{id}/hits', [ 'as' => 'link.hits', 'uses' => 'Dyryme\Controllers\LinkController@hits', ]);
+	Route::get('/', [ 'as' => 'create', 'uses' => 'LinkController@create', ]);
+	Route::post('store', [ 'as' => 'store', 'before' => 'csrf', 'uses' => 'LinkController@store', ]);
+	Route::get('screenshot/{id}', [ 'as' => 'screenshot', 'uses' => 'LinkController@screenshot', ]);
+
+	Route::get('looper', [ 'as' => 'loop_detected', 'uses' => 'LinkController@looper', ]);
+
+	// Authenticated link routes
+	Route::group([ 'prefix' => 'link', 'before' => 'auth', ], function ()
+	{
+		Route::get('list', [ 'as' => 'list', 'uses' => 'LinkController@index', ]);
+		Route::delete('{id}', [ 'as' => 'link.destroy', 'before' => 'auth|csrf', 'uses' => 'LinkController@destroy', ]);
+		Route::put('{id}', [ 'as' => 'link.activate', 'before' => 'auth|csrf', 'uses' => 'LinkController@activate', ]);
+		Route::get('{id}/hits', [ 'as' => 'link.hits', 'uses' => 'LinkController@hits', ]);
+	});
+
+	// Authentication routes
+	Route::get('login', [ 'as' => 'login', 'uses' => 'AuthController@login', ]);
+	Route::post('login', [ 'as' => 'authenticate', 'before' => 'csrf', 'uses' => 'AuthController@authenticate', ]);
+	Route::get('logout', [ 'as' => 'logout', 'uses' => 'AuthController@logout', ]);
+
+	// Registration routes
+	Route::get('register', [ 'as' => 'register', 'uses' => 'RegistrationController@create', ]);
+	Route::post('register', [ 'as' => 'register', 'before' => 'csrf', 'uses' => 'RegistrationController@store', ]);
+
+	// User routes
+	Route::get('/user/links', [ 'as' => 'user.links', 'uses' => 'UserController@links', ]);
+	Route::get('/denied', [ 'as' => 'user.denied', 'uses' => 'UserController@denied', ]);
+
+	// Wildcard redirect routes
+	Route::get('{hash}', [ 'as' => 'redirect', 'uses' => 'LinkController@redirect', ]);
 });
-
-// Authentication routes
-Route::get('login', [ 'as' => 'login', 'uses' => 'Dyryme\Controllers\AuthController@login', ]);
-Route::post('login', [ 'as' => 'authenticate', 'before' => 'csrf', 'uses' => 'Dyryme\Controllers\AuthController@authenticate', ]);
-Route::get('logout', [ 'as' => 'logout', 'uses' => 'Dyryme\Controllers\AuthController@logout', ]);
-
-// Registration routes
-Route::get('register', [ 'as' => 'register', 'uses' => 'Dyryme\Controllers\RegistrationController@create', ]);
-Route::post('register', [ 'as' => 'register', 'before' => 'csrf', 'uses' => 'Dyryme\Controllers\RegistrationController@store', ]);
-
-// User routes
-Route::get('links', [ 'as' => 'user.links', 'uses' => 'Dyryme\Controllers\UserController@links', ]);
-Route::get('denied', [ 'as' => 'user.denied', 'uses' => 'Dyryme\Controllers\UserController@denied', ]);
-
-// Wildcard redirect routes
-Route::get('{hash}', [ 'as' => 'redirect', 'uses' => 'Dyryme\Controllers\LinkController@redirect', ]);
