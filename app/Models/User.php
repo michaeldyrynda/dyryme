6bc235ae8dcb9ba9c664f6319f2a3e3f3b9de7db
Michael Dyrynda <michael@nucleus.com.au>
2015-03-23 16:11:23 +1030

    merge with L5 user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,29 +1,31 @@
 <?php namespace Dyryme\Models;
 
-use Illuminate\Auth\UserTrait;
-use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableTrait;
-use Illuminate\Auth\Reminders\RemindableInterface;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
-
-	use UserTrait, RemindableTrait;
-
+class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
+	use Authenticatable, CanResetPassword;
 	/**
 	 * The database table used by the model.
 	 *
 	 * @var string
 	 */
 	protected $table = 'users';
-
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = ['name', 'email', 'password', 'superuser',];
 	/**
 	 * The attributes excluded from the model's JSON form.
 	 *
 	 * @var array
 	 */
-	protected $hidden = array('password', 'remember_token');
-
-	protected $fillable = [ 'username', 'password', 'superuser', ];
+	protected $hidden = ['password', 'remember_token'];
 
 
 	public static function boot()
