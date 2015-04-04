c53b7c28d2e4809f2526939b9d0ffee6bdf50602
Michael Dyrynda <deringer@iatstuti.net>
2015-04-04 22:27:48 +1030

    bring in views

---
+++ resources/views/_link_row.blade.php
@@ -0,0 +1,47 @@
+<tr @if ( $link->trashed() )class="danger"@endif>
+    <td>
+        @if ( $link->screenshot )<a href="{{ route('screenshot', [ $link->id, ]) }}" target="_blank">@endif
+            <img src="{{ route('screenshot', [ $link->id, 'thumb' => 1, ]) }}" />
+        @if ( $link->screenshot )</a>@endif
+    </td>
+    <td>{{ link_to($link->hash, $link->hash) }} ({{ link_to_route('link.hits', $link->hits->count(), [ $link->id, ]) }})</td>
+    <td><span data-toggle="tooltip" title="{{ $link->url }}">{{ link_to($link->url, str_limit($link->url, 50)) }}</td>
+    <td>
+        @if ( $link->page_title )
+            <span data-toggle="tooltip" title="{{ $link->page_title }}">{{ str_limit($link->page_title, 50) }}</span>
+        @else
+            <span class="text-muted">&ndash;</span>
+        @endif
+    </td>
+    <td>{{ $link->created_at }}</td>
+    <td>
+        {{ $link->remoteAddress or '<span class="text-muted">&ndash;</span>' }}<br>
+        @if ( $link->hostname )
+            <span data-toggle="tooltip" title="{{ $link->hostname }}">{{ str_limit($link->hostname, 50) }}</span>
+        @else
+            <span class="text-muted">&ndash;</span>
+        @endif
+    </td>
+    <td>
+        @if ( $link->userAgent)
+            <span data-toggle="tooltip" title="{{ $link->userAgent }}">{{ str_limit($link->userAgent) }}</span>
+        @else
+            <span class="text-muted">&ndash;</span>
+        @endif
+    </td>
+    <td>
+        @if ( $link->trashed() )
+            {{ Form::open([ 'route' => [ 'link.activate', $link->id, ], 'method' => 'put', ]) }}
+            <button class="btn btn-xs btn-success" title="Activate link">
+                <span class="glyphicon glyphicon-check"></span>
+            </button>
+            {{ Form::close() }}
+        @else
+            {{ Form::open([ 'route' => [ 'link.destroy', $link->id, ], 'method' => 'delete', ]) }}
+            <button class="btn btn-xs btn-danger" title="Delete link">
+                <span class="glyphicon glyphicon-trash"></span>
+            </button>
+            {{ Form::close() }}
+        @endif
+    </td>
+</tr>
---
+++ resources/views/_navigation.blade.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            {{ link_to_route('create', 'dyry.me', null, [ 'class' => 'navbar-brand', ]) }}
+        </div>
+
+        <div class="collapse navbar-collapse" id="navbar-collapse">
+            <ul class="nav navbar-nav">
+                @if ( Auth::check() )
+                    @if ( $authUser->hasPermission('link.list') )<li @if ( Route::currentRouteName() == 'list' ) class="active" @endif>{{ link_to_route('list', 'List') }}</li>@endif
+                    @if ( $authUser->hasPermission('user.links') )<li @if ( Route::currentRouteName() == 'user.links' ) class="active" @endif>{{ link_to_route('user.links', 'My Links') }}</li>@endif
+                @endif
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                @if ( Auth::check() )
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Signed in as {{ Auth::user()->username }} <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menue">
+                            <li><a href="{{ route('logout') }}"><span class="glyphicon glyphicon-off"></span>&nbsp;Logout</a></li>
+                        </ul>
+                    </li>
+                @else
+                    <li>{{ link_to_route('login', 'Login') }}</li>
+                    <li>{{ link_to_route('register', 'Register') }}</li>
+                @endif
+            </ul>
+        </div>
+    </div>
+</nav>
